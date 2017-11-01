e7e5711d3b0f4043ba7c1a48340ea8723e7a9e06
hinst <hinstance@yandex.ru>
2017-11-01 23:57:37 +0300

    compile typescript to one file

--- srv/src/hsite/WebUI.go
+++ srv/src/hsite/WebUI.go
@@ -1,10 +1,34 @@
 package hsite
 
+import (
+	"hgo"
+	"net/http"
+	"sync"
+	"sync/atomic"
+)
+
 type WebUI struct {
+	Holder  sync.WaitGroup
+	Stopped int32
 }
 
 func (this *WebUI) Start() {
 }
 
-func(this *WebUI) Stop() {
+func (this *WebUI) WrapRequestHandler(h hgo.HttpFunc) hgo.HttpFunc {
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		this.Holder.Add(1)
+		defer this.Holder.Done()
+		if atomic.LoadInt32(&this.Stopped) == 0 {
+			h(responseWriter, request)
+		}
+	}
+}
+
+func (this *WebUI) Stop() {
+	atomic.AddInt32(&this.Stopped, 1)
+	this.Holder.Wait()
+}
+
+func (this *WebUI) SetupFiles() {
 }
